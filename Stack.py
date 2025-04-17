3a2f7e66a4acc7009eb08b5027a5d7b8b4fc99c9
shaban <shaban.elaev@mail.ru>
2025-04-17 22:19:03 +0300

    add Stack

---
+++ Stack.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+
+class Stack:
+
+    def __init__(self,):
+
+        self.top = None
+        self.bottom = None
+        self.len = 0
+
+    def is_empty(self):
+        if self.top is None:
+            return  False
+        else:
+            return True
+
+    def peek(self):
+        return self.bottom
+
+    def size(self):
+        return self.len
+
+    def push(self, value):
+        self.len += 1
+        node = Node(value)
+
+        if self.top is None:
+            self.top = node
+            self.bottom = node
+
+        else:
+            node.prev = self.bottom
+            self.bottom = node
+
+    def pop(self):
+
+        if self.top is None:
+            return  None
+
+        else:
+            self.len -= 1
+            result = self.bottom.value
+            self.bottom = self.bottom.prev
+            return result
+
+
+
